f2e77249708c54cfb2d74affa20a9d18811b8f8a
mvcaribe <mvscaribe@gmail.com>
2022-07-04 15:21:39 -0300

    Adicionando evento ao modal, Abrindo e fechando

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let modalQt = 1;
 const c = (el)=>document.querySelector(el);
 const cs = (el)=>document.querySelectorAll(el);
 
+// listagem das pizzas
 pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
     // preencher as informações em pizzaitem
@@ -38,4 +39,16 @@ pizzaJson.map((item, index)=>{
    });
 
     c('.pizza-area').append( pizzaItem );
+});
+
+
+// Eventos do modal
+function closeModal() {
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none';
+    }, 500);
+}
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal);
 });
